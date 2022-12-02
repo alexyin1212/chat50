268337aa709013b8e5b5742a449fc8ba3235096d
Alex Yin <alex_yin1@hotmail.com>
2022-12-02 14:09:20 -0500

    after hackathon

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def index():
 def my_posts():   
     id = session["user_id"]
     posts = db.execute("SELECT * FROM posts WHERE user_id = ? ORDER BY time DESC", id)
-    return render_template("index.html", posts=posts)
+    return render_template("my_posts.html", posts=posts)
 
 
 @app.route("/post", methods=["GET", "POST"])

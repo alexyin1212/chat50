8f880aac78c60ec74a313e66da2f8bf495247f8d
Bridget Ma <bridgetma@dhcp-10-250-163-102.harvard.edu>
2022-12-06 22:30:39 -0500

    doneeeee

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def index():
     posts = db.execute("SELECT * FROM posts ORDER BY time DESC")
     liked = db.execute("SELECT post_id FROM liked WHERE user_id = ?", id)
     disliked = db.execute("SELECT post_id FROM disliked WHERE user_id = ?", id)
-    # create arrays and adds all the liked and disliked posts to them
+    # create lists and adds all the liked and disliked posts of this user to them
     likes = []
     dislikes = []
     for post in liked:
@@ -64,6 +64,7 @@ def index():
 @login_required
 def top():   
     id = session["user_id"]
+    # just like the index page but the votes are grabed in likes descending order from database
     posts = db.execute("SELECT * FROM posts ORDER BY likes DESC")
     liked = db.execute("SELECT post_id FROM liked WHERE user_id = ?", id)
     disliked = db.execute("SELECT post_id FROM disliked WHERE user_id = ?", id)
@@ -81,30 +82,48 @@ def top():
 @app.route("/likes", methods=["GET", "POST"])
 @login_required
 def likes():
+    # checks for the post from ajax
     if request.method == "POST":
+        # create an empty dict
         data = {}
+        # grab the data from ajax and place them into the dict we created 
         data["id"] = request.json["post_id"]
         data["likes"] = int(request.json["likes"])
         data["user_id"] = request.json["user_id"]
         db.execute("UPDATE posts SET likes = ? WHERE id = ?", data["likes"], data["id"])
         data["status"] = request.json["status"]
+        # get all the liked and disliked posts of the user from the database
         liked = db.execute("SELECT * FROM liked WHERE post_id = ? AND user_id = ?", data["id"], data["user_id"])
         disliked = db.execute("SELECT * FROM disliked WHERE post_id = ? AND user_id = ?", data["id"], data["user_id"])
+        # if the user clicked on the like button
         if data["status"] == "liked":
+                # if the post is already liked by the user
                 if liked:
+                    # remove the user-post pair from the liked database
                     db.execute("DELETE FROM liked WHERE post_id = ? AND user_id = ?", data["id"], data["user_id"])
+                # if the post is already disliked by the user
                 elif disliked:
+                    # remove the user-post pair from the disliked database and add the pair to the liked database
                     db.execute("DELETE FROM disliked WHERE post_id = ? AND user_id = ?", data["id"], data["user_id"])
                     db.execute("INSERT INTO liked (post_id, user_id) VALUES (?, ?)", data["id"], data["user_id"])
+                # if no button is currently on
                 else:
+                    # add the pair to the liked database
                     db.execute("INSERT INTO liked (post_id, user_id) VALUES (?, ?)", data["id"], data["user_id"])
+        # if the user clicked on the disliked button
         else:
+                # if the post is already disliked by the user
                 if disliked:
+                    # remove the user-post pair from the disliked database
                     db.execute("DELETE FROM disliked WHERE post_id = ? AND user_id = ?", data["id"], data["user_id"])
+                # if the post is already liked by the user
                 elif liked:
+                    # remove the user-post pair from the liked database and add the pair to the disliked database
                     db.execute("DELETE FROM liked WHERE post_id = ? AND user_id = ?", data["id"], data["user_id"])
                     db.execute("INSERT INTO disliked (post_id, user_id) VALUES (?, ?)", data["id"], data["user_id"])
+                # if no button is currently on
                 else:
+                    # add the pair to the disliked database
                     db.execute("INSERT INTO disliked (post_id, user_id) VALUES (?, ?)", data["id"], data["user_id"])
 
         return redirect("/")
@@ -116,17 +135,21 @@ def likes():
 @app.route("/my_posts")
 @login_required
 def my_posts():
+    # get all the data relevent from the databases
     id = session["user_id"]
     posts = db.execute("SELECT * FROM posts WHERE user_id = ? ORDER BY time DESC", id)
+    # get the liked and disliked posts from by the user
     liked = db.execute("SELECT post_id FROM liked WHERE user_id = ?", id)
     disliked = db.execute("SELECT post_id FROM disliked WHERE user_id = ?", id)
     likes = []
     dislikes = []
+    # change the list of dicts into lists of post_id
     for post in liked:
         likes.append(post["post_id"])
 
     for post in disliked:
         dislikes.append(post["post_id"])
+    # pass the data to the profile page
     return render_template("my_posts.html", posts=posts, likes=likes, dislikes=dislikes)
 
 
@@ -134,11 +157,14 @@ def my_posts():
 @app.route("/post", methods=["GET", "POST"])
 @login_required
 def post():
+    # check to see if the user is submitting a post
     if request.method == "POST":
         id = session["user_id"]
         post = request.form.get("post")
+        # insert the new post into the database
         db.execute("INSERT INTO posts (user_id, post) VALUES (?, ?)", id, post)
         return redirect("/")
+    # return the posting page
     else:
         return render_template("post.html")
 
@@ -257,6 +283,7 @@ def change_password():
         db.execute("UPDATE users SET hash = ? WHERE id = ?", hashed_password, session["user_id"])
         return redirect("/login")
 
+    # return the form to submit new password
     if request.method == "GET":
         return render_template("change_password.html")
 
@@ -264,22 +291,28 @@ def change_password():
 @app.route("/change_username", methods=["GET", "POST"])
 @login_required
 def change_username():
+    # check to see if user is submitting data
     if request.method == "POST":
         new_username = request.form.get("new_username")
         confirmation = request.form.get("confirmation")
+        # check if username is null
         if not new_username or not confirmation:
             return apology("must type in a username!")
+        # check if confirmation and new username match
         if new_username != confirmation:
             apology("usernames don't match")
         current_username = db.execute("SELECT username FROM users")
+        # check if new username is the same as old username
         if new_username == current_username:
             apology("Your new username is the same as your old ")
         existing_users = db.execute("SELECT username FROM users")
-
+        # check if new username already exists in the database
         if new_username in [user["username"] for user in existing_users]:
             return apology("this username already exists")
+        # if everything checks out then update the username in the database
         db.execute("UPDATE users SET username = ? WHERE id = ?", new_username, session["user_id"])
         return redirect("/")
 
+    # return the form to submit new username
     if request.method == "GET":
         return render_template("change_username.html")
--- static/function.js
+++ static/function.js
@@ -1,38 +1,55 @@
 function like(id, user_id) {
+    // set variables for the like and dislike button
     let like = document.getElementById("like" + id);
     let dislike = document.getElementById("dislike" + id);
+    // set variables for the text (number of karma)
     let counter = document.getElementById("counter" + id)
+    // set int varible for the number of karma
     let count = parseInt(counter.innerHTML);
+    // set state of like and dislike
     let liked = false;
     let disliked = false;
 
+    // if the liked button is purple
     if (like.classList.contains("on")) {
+        // liked status is true
         liked = true;
     }
+    // if the disliked button is purple
     else if (dislike.classList.contains("on")) {
+        // disliked status is true
         disliked = true;
     }
 
+    // if both buttons are off
     if (liked == false && disliked == false) {
+        // make like button purple and increment counter by 1 then set the innerhtml to the counter
         like.classList.add("on");
         count++;
         counter.innerHTML = count;
     }
+    // if like button is off but dislike button is on
     else if (liked == false && disliked == true) {
+        // make like button purple, dislike button back to grey stage and increment counter by 2 then set the innerhtml to the counter
         like.classList.add("on");
         dislike.classList.remove("on");
         count += 2;
         counter.innerHTML = count;
     }
+    // if like button is on
     else {
+        // make like button back to grey stage and decrease counter by 1 then set the innerhtml to the counter
         like.classList.remove("on");
         count--;
         counter.innerHTML = count;
     }
 
-    var dict = {"likes": count, "post_id": id, "user_id": user_id, "status": "liked"};    
+    // create a dictionary to pass to json
+    var dict = {"likes": count, "post_id": id, "user_id": user_id, "status": "liked"};
+    // stringify the dict     
     var dataJson = JSON.stringify(dict);
     
+    // use ajax syntax to post the data to /likes
     $.ajax({
         url: "/likes",
         type: "POST",
@@ -45,40 +62,57 @@ function like(id, user_id) {
 
 
 function dislike(id, user_id) {   
+    // set variables for the like and dislike button
     let like = document.getElementById("like" + id);
     let dislike = document.getElementById("dislike" + id);
+    // set variables for the text (number of karma)
     let counter = document.getElementById("counter" + id)
+    // set int varible for the number of karma
     let count = parseInt(counter.innerHTML);
+    // set state of like and dislike
     let liked = false;
     let disliked = false;
 
+    // if the liked button is purple
     if (like.classList.contains("on")) {
+        // liked status is true
         liked = true;
     }
+    // if the disliked button is purple
     else if (dislike.classList.contains("on")) {
+        // disliked status is true
         disliked = true;
     }
 
+     // if both buttons are off
     if (liked == false && disliked == false) {
+        // make dislike button purple and decrease counter by 1 then set the innerhtml to the counter
         dislike.classList.add("on");
         count--;
         counter.innerHTML = count;
     }
+    // if like button is on but dislike button is off
     else if (liked == true && disliked == false) {
+        // make dislike button purple, like button back to grey stage and decrease counter by 2 then set the innerhtml to the counter
         dislike.classList.add("on");
         like.classList.remove("on");
         count -= 2;
         counter.innerHTML = count;
     }
+    // if dislike button is on
     else {
+        // make dislike button back to grey stage and increment counter by 1 then set the innerhtml to the counter
         dislike.classList.remove("on");
         count++;
         counter.innerHTML = count;
     }
 
+    // create a dictionary to pass to json
     var dict = {"likes": count, "post_id": id, "user_id": user_id, "status": "disliked"};
+    // stringify the dict     
     var dataJson = JSON.stringify(dict);
-    
+
+    // use ajax syntax to post the data to /likes
     $.ajax({
         url: "/likes",
         type: "POST",
